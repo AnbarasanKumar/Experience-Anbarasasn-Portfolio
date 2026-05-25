130adb157df747f417438bb2302f2872a5eb927d
AnbarasanKumar <anbarasanpno18@gmail.com>
2026-05-25 11:02:57 +0530

    ui: remove CustomCursor from AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -15,7 +15,6 @@ import FloatingWhatsAppButton from './FloatingWhatsAppButton';
 
 import { motion, useScroll, useSpring } from 'framer-motion';
 
-import CustomCursor from './CustomCursor';
 
 const AppLayout: React.FC = () => {
   const { scrollYProgress } = useScroll();
@@ -32,7 +31,6 @@ const AppLayout: React.FC = () => {
       transition={{ duration: 1.2 }}
       className="min-h-screen relative overflow-x-hidden bg-[#f8fafc]"
     >
-      <CustomCursor />
       <div className="fixed inset-0 z-[-1] opacity-5 pointer-events-none bg-[url('https://www.transparenttextures.com/patterns/white-diamond.png')]" />
       <motion.div
         className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-teal-500 via-indigo-500 to-teal-500 z-[100] origin-left"
